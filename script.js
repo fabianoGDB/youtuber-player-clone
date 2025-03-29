2db63e0bed0bb2100c580e6736a7d37d22a57a61
fabianoGDB <doctorfabi.f@gmail.com>
2025-03-29 20:56:08 -0300

    adding theater, fullscreen and miniplayer modes

---
+++ script.js
@@ -0,0 +1,93 @@
+const theaterBtn = document.querySelector(".theater-btn")
+const fullScreenBtn = document.querySelector(".full-screen-btn")
+const miniPlayerBtn = document.querySelector(".mini-player-btn")
+const playPauseBtn = document.querySelector(".play-pause-btn")
+const video = document.querySelector('video')
+const videoContainer = document.querySelector('.video-container')
+
+
+
+document.addEventListener("keydown", e => {
+    const tagName = document.activeElement.tagName.toLowerCase()
+
+    if (tagName === "input") return
+    switch (e.key.toLowerCase()){
+        case " ":
+            if (tagName === "button") return
+        case "k":
+            togglePlay()
+            break
+        case "f":
+            toggleFullScreenMode()
+            break
+        case "t":
+            toggleTheaterMode()
+            break
+        case "i":
+            toggleMiniPlayerMode()
+            break
+    }
+})
+
+
+
+// View Models
+theaterBtn.addEventListener("click", toggleTheaterMode)
+fullScreenBtn.addEventListener("click", toggleFullScreenMode)
+miniPlayerBtn.addEventListener("click", toggleMiniPlayerMode)
+
+function toggleTheaterMode(){
+    videoContainer.classList.toggle("theater")
+
+}
+
+function toggleFullScreenMode(){
+    if(document.fullscreenElement == null){
+        videoContainer.requestFullscreen()
+    }
+    else{
+        document.exitFullscreen()
+    }
+    
+}
+
+function toggleMiniPlayerMode(){
+    if(videoContainer.classList.contains("mini-player")){
+        document.exitPictureInPicture()
+    }
+    else{
+        video.requestPictureInPicture()
+    }
+}
+
+document,addEventListener("fullscreenchenge", () => {
+    videoContainer.classList.toggle("full-screen", document.fullscreenElement)
+})
+
+
+video.addEventListener("enterpictureinpicture", () => {
+    videoContainer.classList.add("mini-player")
+})
+
+
+video.addEventListener("leavepictureinpicture", () => {
+    videoContainer.classList.remove("mini-player")
+})
+
+// Play/ Pause
+playPauseBtn.addEventListener('click', togglePlay)
+video.addEventListener("click", togglePlay)
+
+function togglePlay(){
+    video.paused ? video.play() : video.pause()
+}
+
+
+video.addEventListener("play", () => {
+    videoContainer.classList.remove("paused")
+})
+
+
+video.addEventListener("pause", () => {
+    videoContainer.classList.add("paused")
+})
